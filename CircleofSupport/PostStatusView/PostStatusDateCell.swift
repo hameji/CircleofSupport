31a5296e0b61463f2f24cc77c3be3cea8e46d887
Hajime Taniguchi <hameji@gmail.com>
2019-10-23 14:20:23 +0900

    custom cellのファイルを作成

---
+++ CircleofSupport/PostStatusView/PostStatusDateCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostStatusDateCell.swift
+//  CircleofSupport
+//
+//  Created by Hajime Taniguchi on 2019/10/23.
+//  Copyright © 2019 Hajime Taniguchi. All rights reserved.
+//
+
+import Foundation
