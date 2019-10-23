d4694b63261571b0ec70235285241b5afe79630e
Hajime Taniguchi <hameji@gmail.com>
2019-10-23 14:16:36 +0900

    presentCellを作成

---
+++ CircleofSupport/PostStatusView/PostStatusPresentCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostStatusPresentCell.swift
+//  CircleofSupport
+//
+//  Created by Hajime Taniguchi on 2019/10/23.
+//  Copyright © 2019 Hajime Taniguchi. All rights reserved.
+//
+
+enum PostStatusPresentCell {
+    case dateCell
+    case addressCell(PostStatusAddressData)
+    case lightCell
+    case gassCell
+    case waterCell
+}
