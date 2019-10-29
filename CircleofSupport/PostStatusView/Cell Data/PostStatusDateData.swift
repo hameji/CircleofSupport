5060bbff2ace7020c0941b59fc4dc7b56b074fdb
Hajime Taniguchi <hameji@gmail.com>
2019-10-29 14:26:55 +0900

    必要なCellDataを追加

---
+++ CircleofSupport/PostStatusView/Cell Data/PostStatusDateData.swift
@@ -0,0 +1,11 @@
+//
+//  PostStatusDateData.swift
+//  CircleofSupport
+//
+//  Created by Hajime Taniguchi on 2019/10/29.
+//  Copyright © 2019 Hajime Taniguchi. All rights reserved.
+//
+
+struct PostStatusDateData {
+    let date: Double
+}
---
+++ CircleofSupport/PostStatusView/Cell Data/PostStatusLifeLineData.swift
@@ -0,0 +1,12 @@
+//
+//  PostStatusLifeLineData.swift
+//  CircleofSupport
+//
+//  Created by Hajime Taniguchi on 2019/10/29.
+//  Copyright © 2019 Hajime Taniguchi. All rights reserved.
+//
+
+struct PostStatusLifeLineData {
+    let isSelected: Bool
+    let description: String
+}
