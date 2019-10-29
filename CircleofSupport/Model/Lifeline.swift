c15dee2f5117786afc8d8abff71be18679b5ab79
Hajime Taniguchi <hameji@gmail.com>
2019-10-29 21:52:47 +0900

    データLifelineをstructで作成

---
+++ CircleofSupport/Model/Lifeline.swift
@@ -0,0 +1,23 @@
+//
+//  Lifeline.swift
+//  CircleofSupport
+//
+//  Created by Hajime Taniguchi on 2019/10/29.
+//  Copyright © 2019 Hajime Taniguchi. All rights reserved.
+//
+
+import Foundation
+
+struct Lifeline {
+    let lifelineID: String
+    let prefecture: String
+    let city: String
+    let latitude: Double
+    let longitude: Double
+    let light: Bool
+    let gass: Bool
+    let water: Bool
+    let registerDate: Date
+    let registerUsr: String
+    let verified: Bool
+}
